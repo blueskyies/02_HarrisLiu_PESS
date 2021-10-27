ac384ac13bad098a7c02bafa7a71c086de577b04
blueskyies <harrisliu1234@live.com.sg>
2021-10-28 02:52:47 +0800

    First BackUp
    Part 5 Backup

---
+++ nav.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="HarrisLiuStyle.css" type="text/css">
+</head>
+<body>
+
+<img src="Banner.png" title="Banner" alt="Banner" class="center" />
+<h1 style="text-align:center">Police Emergency Service System</h1>
+<nav>
+	<li><a href="logcall.php">Log Call</a></li>
+    <li><a href="Update.php">Update</a></li>
+	<li><a href="#">Report</a></li>
+	<li><a href="#">History</a></li>
+ </nav>
+	
+</body>
+</html>
